da9f49b6483958d2ac77be990804e669aea83874
s1280189_Tanagi <86813225+s1280189@users.noreply.github.com>
2022-10-14 16:02:15 +0900

    Create taskA_re1.c

---
+++ ex2/taskA_re1.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+int main(){
+  FILE *fp;
+  char fname[]="height.txt";
+  int nx=1000,ny=1000;
+  float data[nx][ny];
+  float f_data;
+  int nxy_number[2];
+  char str[nx][ny];
+  char* ptr;
+  int count=0;
+  float result=0;
+
+ 
+  
+  fp=fopen(fname, "r");
+  if(fp==NULL){
+    printf("%s file not open\n",fname);
+    return -1;
+  }
+
+  fscanf(fp, "%f", nx);
+  fscanf(fp, "%f", ny);
+ for(int i=0;i<nx;i++){
+    for(int j=0;j<ny;j++){
+      fscanf(fp, "%s", str[i]);
+    }
+  }
+  int j=0;
+  for(int i=0;i<nx;i++){
+    ptr=strtok(str[i],",");
+    data[i][j]=strtof(ptr,NULL);
+    j++;
+    while(ptr != NULL){
+      ptr = strtok(NULL, ",");
+      data[i][j]=strtof(ptr,NULL);
+      j++;
+    }
+    j=0;
+  }
+  fclose(fp);
+  
+  //x-direction
+  for(int i=0;i<nx;i++){
+    for(int j=0;j<ny;j++){
+      result=result+data[i][j];
+    }
+  }
+  printf("x-direction: %f\n",result);
+
+  //y-direction
+  result=0;
+  for(int j=0;j<ny;j++){
+    for(int i=0;i<nx;i++){
+      result=result+data[i][j];
+    }
+  }
+  printf("y-direction: %f\n",result);
+
+  
+
+  return 0;
+}
